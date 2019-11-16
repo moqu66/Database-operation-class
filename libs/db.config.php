769644866e7e67a94b3cc528b8d8f8f044a96b2d
LIttleMo <56909488+moqu66@users.noreply.github.com>
2019-11-16 12:49:09 +0800

    数据库配置文件

---
+++ libs/db.config.php
@@ -0,0 +1,9 @@
+<?php
+# 数据库配置文件
+define('DBtype','mysql');
+define('DBhost','localhost');
+define('DBname','root');
+define('DBuser','root');
+define('DBpass','123456');
+define('DBport',"3306");
+define('DEBUGGING',true);
